60ccf8244bab91d355e05f00784e9f43002371e9
Andrus Asumets <andrus.asumets@gmail.com>
2017-06-18 17:41:12 +0200

    headless chrome

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const IO = require('koa-socket')
 const io = new IO()
 
 const PORT = process.env.PORT || 1337
+let buffer = null
 
 app.use(bodyParser())
 app.use(cors())
@@ -19,6 +20,8 @@ io.attach(app)
 
 io.on('connection', (context, data) => {
 	console.log('new connection')
+	
+	io.broadcast('buffer', buffer)
 })
 
 io.on('message', (context, data) => {
@@ -28,3 +31,23 @@ io.on('message', (context, data) => {
 app.listen(PORT)
 
 console.log('Server is listening on', PORT + '.')
+
+
+(async function() {
+	const chrome = await launchChrome();
+	const protocol = await CDP({port: chrome.port})
+	
+	// Extract the DevTools protocol domains we need and enable them.
+	// See API docs: https://chromedevtools.github.io/devtools-protocol/
+	const {Page, Runtime} = protocol
+	await Promise.all([Page.enable(), Runtime.enable()])
+	
+	Page.navigate({url: 'http://localhost:1337/'})
+
+	Page.loadEventFired(async () => {
+		setInterval(function() {
+			const screenshot = await Page.captureScreenshot({'png'})
+			buffer = new Buffer(screenshot.data, 'base64')
+		}, 100)
+	})
+})()
